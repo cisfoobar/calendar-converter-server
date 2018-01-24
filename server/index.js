e5071fe3bac9334f172db1c88a64ac2286dff7a8
Falafel72 <ksh717@student.cis.edu.hk>
2018-01-24 20:44:24 +0800

    Added moongate scraper.
    The scraper first logs in, searches for the
    name, then gets the timetable for the first student result.
    
    Edge cases for teachers and students whose results are not first need to be added. Error handling also still needs to be implemented.

--- server/index.js
+++ server/index.js
@@ -3,13 +3,59 @@ const puppeteer = require("puppeteer");
 const app = express();
 require("dotenv").config();
 
+/*
+TO-DO: 
+  - edge cases: 
+    - yourself
+    - teachers
+    - people who are not the first result from search
+  - parse the table
+  - error handling
+*/
+
 app.get("/", async (req, res) => {
   const name = req.query.name; // name to scrape for
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://google.com/?q=" + name);
-  await page.screenshot({ path: "example.png" });
-  res.send("Hello World!");
+
+  //go to moongate
+  await page.goto("https://moongate.cis.edu.hk");
+
+  //login
+  await page.type("input#ctl00_cph_pagecontent_Login1_UserName",process.env.MOONGATE_USERNAME,{delay:100});
+  await page.type("input#ctl00_cph_pagecontent_Login1_Password",process.env.MOONGATE_PASSWORD,{delay:100});
+  await page.click("input#ctl00_cph_pagecontent_Login1_LoginButton");
+  await page.waitForNavigation();
+
+  //search for the name
+  await page.type("input#kw",name,{delay:100});
+  await page.click("input#search-btn");
+  await page.waitForNavigation();
+  //person we want may not be the first result
+
+  //click on results - students or results - faculty/staff
+  page.click("div.views_horisontal ul li:nth-child(3) a");
+  await page.waitForNavigation();
+
+  //navigate to their profile
+  page.click("table.weblist tr:first-child a");
+  await page.waitForNavigation();
+
+  //navigate to their timetable
+  page.click("div#tabspagespanel ul:nth-child(3) a");
+  await page.waitForNavigation();
+  
+  //grab the table (doesn't work if you do your own timetable)
+  await page.waitFor("#divsection_578FB9CE-05B6-4405-BCEE-07E689546B68 > div > table");
+  await page.screenshot({path: '/Users/kylehu/Desktop/test.png'});
+  
+  //need to use native js functions here because the ones provided in puppeteer don't seem to work
+  const tableHTML = await page.evaluate((sel) => {
+    let table = document.querySelector("#divsection_578FB9CE-05B6-4405-BCEE-07E689546B68 > div > table");
+    return table ? table.outerHTML : null;
+  });
+  res.send(tableHTML);
+
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
