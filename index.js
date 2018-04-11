7990cfb07b99f97e65d000c1fdff06d58e0039f7
Daniel Hsing <hsing.daniel@gmail.com>
2018-04-11 10:13:39 +0800

    feat: initialize parse_dates js module. add helper function for retrieving date array
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- index.js
+++ index.js
@@ -18,19 +18,27 @@ app.get("/", async (req, res) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  console.log(name)
+  console.log(name);
 
   //go to moongate
   await page.goto("https://moongate.cis.edu.hk");
 
   //login
-  await page.type("input#ctl00_cph_pagecontent_Login1_UserName",process.env.MOONGATE_USERNAME,{delay:100});
-  await page.type("input#ctl00_cph_pagecontent_Login1_Password",process.env.MOONGATE_PASSWORD,{delay:100});
+  await page.type(
+    "input#ctl00_cph_pagecontent_Login1_UserName",
+    process.env.MOONGATE_USERNAME,
+    { delay: 100 }
+  );
+  await page.type(
+    "input#ctl00_cph_pagecontent_Login1_Password",
+    process.env.MOONGATE_PASSWORD,
+    { delay: 100 }
+  );
   await page.click("input#ctl00_cph_pagecontent_Login1_LoginButton");
   await page.waitForNavigation();
 
   //search for the name
-  await page.type("input#kw",name,{delay:100});
+  await page.type("input#kw", name, { delay: 100 });
   await page.click("input#search-btn");
   await page.waitForNavigation();
   //person we want may not be the first result
@@ -46,22 +54,21 @@ app.get("/", async (req, res) => {
   //navigate to their timetable
   page.click("div#tabspagespanel ul:nth-child(3) a");
   await page.waitForNavigation();
-  
+
   //grab the table (doesn't work if you do your own timetable)
-  await page.waitFor("#divsection_578FB9CE-05B6-4405-BCEE-07E689546B68 > div > table");
-  
+  await page.waitFor(
+    "#divsection_578FB9CE-05B6-4405-BCEE-07E689546B68 > div > table"
+  );
+
   //need to use native js functions here because the ones provided in puppeteer don't seem to work
-  const tableHTML = await page.evaluate((sel) => {
-    let table = document.querySelector("#divsection_578FB9CE-05B6-4405-BCEE-07E689546B68 > div > table");
+  const tableHTML = await page.evaluate(sel => {
+    let table = document.querySelector(
+      "#divsection_578FB9CE-05B6-4405-BCEE-07E689546B68 > div > table"
+    );
     return table ? table.outerHTML : null;
   });
+  console.log("test");
   res.send(tableHTML);
-  let file_contents = "Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n"
-  const dayCycles = {
-    "a1": [{subject: "11 PE 2 1718", period: 1, teacher: "Stephen May", room_number: "001"}],
-    "a2": []
-  }
-
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
